80855266befeac5317342266366113f42cadf12c
allenyllee <allen7575@gmail.com>
2017-11-02 22:35:46 +0800

    build dlib

--- notebooks/install_dlib.sh
+++ notebooks/install_dlib.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
-# build dlib
-sudo apt-get install -y build-essential cmake libboost-python-dev
+# Install Dlib on Ubuntu | Learn OpenCV 
+# https://www.learnopencv.com/install-dlib-on-ubuntu/
+#
+# dependencies for build dlib: cmake, boost, PkgConfig
+# build-essential: gcc,g++,make...
+# check dependency: apt-cache depends build-essential
+sudo apt-get install -y build-essential cmake libboost-python-dev, pkg-config
 
-# X11
+# dependencies for X11 GUI window
 sudo apt-get install -y libx11-dev
 
-pip install dlib
+# build dlib
+# davisking/dlib: A toolkit for making real world machine learning and data analysis applications in C++ 
+# https://github.com/davisking/dlib
+bash <<EOF
+cd /root/project
+git clone https://github.com/davisking/dlib.git
+cd dlib/
+python setup.py install
+# python setup.py install --yes USE_AVX_INSTRUCTIONS
+EOF
+
