9163cdd4592b94b6c1c21c6d79ceceb94f05b031
allenyllee <allen7575@gmail.com>
2017-11-09 15:44:01 +0800

    update git modules

--- notebooks/ImageProcessig/add_submodule.sh
+++ notebooks/ImageProcessig/add_submodule.sh
@@ -3,5 +3,5 @@
 #
 # add items that you want it be a submodule
 #
-git submodule add https://github.com/yuhanyen/Face_SWAP_python.git
-git submodule add https://github.com/yuhanyen/Emotion_Recognition_python.git
+git submodule add https://github.com/allenyllee/Face_SWAP_python.git
+git submodule add https://github.com/allenyllee/Emotion_Recognition_python.git
